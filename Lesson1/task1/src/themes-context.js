6c87be653ddb9a9433bcb64f0c8b808f199b4162
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-07-16 17:32:34 +0300

    t1

---
+++ Lesson1/task1/src/themes-context.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const themes = {
+  dark: {
+    fontColor: "#ffffff",
+    background: "#222222",
+  },
+  light: {
+    fontColor: "#000",
+    background: "#eee",
+  },
+};
+
+export const ThemeContext = React.createContext({});
